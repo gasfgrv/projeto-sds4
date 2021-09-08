60948474b59c1d4a909ed0319cffb1191d9504ae
Gustavo Silva <gustavo_almeida11@hotmail.com>
2021-09-08 20:43:37 -0300

    graficos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,36 @@
+import BarChart from 'components/BarChart';
 import DataTable from 'components/DataTable';
+import DonutChart from 'components/DonutChart';
 import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <div className="container">
-        <h1 className="text-primary">Olá mundo</h1>
-        <DataTable />
-      </div>
-        <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<div className="container">
+				<h1 className="text-primary">Dashboard</h1>
+
+				<div className="row px-3 py-3">
+					<div className="col-sm-6">
+						<h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+						<BarChart />
+					</div>
+					<div className="col-sm-6">
+						<h5 className="text-center text-secondary">Todas as vendas</h5>
+						<DonutChart />
+					</div>
+				</div>
+
+				<div className="py-3">
+					<h2 className="text-primary">Todas as vendas</h2>
+				</div>
+
+				<DataTable />
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
