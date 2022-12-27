fa354c9c21ca3f6489c179ab705594ddd5f3d1fe
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-12-26 22:43:26 -0300

    dockerfile do frontend

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 
 export const round = (value: number, precision: number) => {
-    var multiplier = Math.pow(10, precision || 0);
+    const multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
 }
 
